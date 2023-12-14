3e6c2f987621af43ec9b5840754e6c5482d5d70b
Rita Oleynik <rita.oleynik99@gmail.com>
2023-12-14 18:40:51 +0300

    lesson-02/05.js

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport_new = {
+    name: "Ivan",
+    surname: "Petrov",
+};
+
+console.log(passport.name);
+console.log(passport_new.name);
