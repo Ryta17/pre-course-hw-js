93bc39e7bcaa3e2c663c9386f8e785a01ac1e393
Rita Oleynik <rita.oleynik99@gmail.com>
2023-12-14 21:28:28 +0300

    lesson-02/07.js

---
+++ lesson-02/07.js
@@ -0,0 +1,21 @@
+let passportMarried = {
+    married: true,
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried_new = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+console.log(passportMarried);
+console.log(passportMarried_new);
