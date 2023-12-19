8fb2a80fb47717fdcdc2448bed99be91d577f41e
Rita Oleynik <rita.oleynik99@gmail.com>
2023-12-20 00:16:20 +0300

    lesson-03/01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,15 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    let a = colors.filter(function(elem) {
+        if (elem == "черный" || elem == "красный" || elem == "желтый") {
+            return true;
+        } else {
+            return false;
+        }
+    })
+    return a.join('-');
+}
+
+console.log(createColorString());
+
